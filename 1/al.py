947e554a68b0f559422242b879143eaed43d64c2
almsp <120767170+almsp@users.noreply.github.com>
2023-04-28 01:19:00 +0600

    Add files via upload

---
+++ 1/al.py
@@ -0,0 +1,25 @@
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
+
+# Тест 1
+graph = {0: {1, 2}, 1: {2}, 2: {0, 3}, 3: {3}}
+assert bfs(graph, 2) == {0, 1, 2, 3}
+
+# Тест 2
+graph = {0: {1, 2}, 1: {0, 2}, 2: {0, 1, 3}, 3: {2}}
+assert bfs(graph, 0) == {0, 1, 2, 3}
+
+# Тест 3
+graph = {0: {1, 2}, 1: {2}, 2: {0, 3}, 3: {1}}
+assert bfs(graph, 2) == {0, 1, 2, 3}
+
+# Тест 4
+graph = {0: set(), 1: {2}, 2: {1}}
+assert bfs(graph, 0) == {0}
